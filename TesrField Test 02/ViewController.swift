76eb9626d915fbd1efcaef4576bb9a4030844cb0
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-03-21 14:29:33 +0900

    .

--- TesrField Test 02/ViewController.swift
+++ TesrField Test 02/ViewController.swift
@@ -1,25 +1,32 @@
-//
-//  ViewController.swift
-//  TesrField Test 02
-//
-//  Created by D7702_09 on 2018. 3. 19..
-//  Copyright © 2018년 lyw. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //delegate 연결
+     myTextField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonPressed(_ sender: Any) {
+        myLabel.text = myTextField.text
+        myTextField.text = ""
+        myTextField.resignFirstResponder()
     }
-
-
+    
+    // UItextFiledDelegate method
+    // called when 'return' key pressed. return NO to ignore.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("I pressed return Key")
+        // 키패드를 내림
+        myTextField.resignFirstResponder()
+        return true
+    }
 }
 
